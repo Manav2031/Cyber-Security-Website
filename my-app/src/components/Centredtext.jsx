adc9a176f4c601a306fb6e0412617793ee89f742
Manav2031 <manavmehta2031@gmail.com>
2024-03-26 22:24:21 +0530

    first commit

---
+++ my-app/src/components/Centredtext.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Centredtext.css'
+
+const Centredtext = () => {
+  return (
+    <div className="centered-text">
+      <h1>The Future of Marketing: <br /> Predictions for the Next 5 Years</h1>
+    </div>
+  )
+}
+
+export default Centredtext
---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import backgroundImage from "../images/cybersec2.jpg";
+import Navbar from "../components/Navbar";
+import Centredtext from "../components/Centredtext";
+import "../styles/Navbar.css";
+import "../styles/Home.css";
+
+const Home = () => {
+  return (
+    <div
+      className="home-container"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <Navbar />
+      <Centredtext />
+      <div className="left-aligned-text">
+        <h2>AAA'S OF CYBER SECURITY</h2>
+        <br />
+        <h3>
+          {" "}
+          Over the past decade, marketing has undergone a significant <br />
+          transformation, driven by technological advancements and consumer{" "}
+          <br />
+          behavior changes. For the next five years, we have made the following{" "}
+          <br />
+          projections for the marketing industry.
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
