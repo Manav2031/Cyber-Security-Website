e6bd74f88918425531c702c07f4f0a1666c42511
Manav2031 <manavmehta2031@gmail.com>
2024-03-27 01:10:26 +0530

    minor changes

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -10,16 +10,17 @@ const Home = () => {
       <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
       <Centredtext />
       <div className="left-aligned-text">
-        <h2>AAA'S OF CYBER SECURITY</h2>
-        <br />
+        <h2> AAA OF SECURITY (Authentication, Authorization, and Accounting) </h2>
+        <br /> <br />
         <h3>
-          {" "}
-          Over the past decade, marketing has undergone a significant <br />
-          transformation, driven by technological advancements and consumer{" "}
-          <br />
-          behavior changes. For the next five years, we have made the following{" "}
-          <br />
-          projections for the marketing industry.
+        AAA, which stands for Authentication, Authorization, and Accounting, is the foundation of strong security systems in many computing environments. 
+        <br /> <br />
+Authentication ensures that users or entities attempting to access systems or resources are who they claim to be. It guarantees that only authorized users or devices obtain access, generally using passwords, biometrics, or security tokens. Effective authentication techniques guard against unwanted access and protect sensitive data from compromise. 
+<br /> <br />
+Authorization specifies what authenticated users can perform within a system or network. It defines access permissions and privileges according to established rules or policies. Authorization methods enforce limits, ensuring that users only interact with resources that they have permission to access. Authorization helps to avoid illegal activity and data breaches by restricting user privileges. 
+<br /> <br />
+Accounting entails tracking and recording user behaviors.
+<br /> <br />
         </h3>
       </div>
     </div>
